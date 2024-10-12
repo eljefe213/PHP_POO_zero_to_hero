8ed40b166b5a5f87e142b0ed5b2e96a90da592ae
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-10-12 20:04:02 +0200

    WIP: Update the paypal payment class & the user class to define the correct namespaces

--- Class/Paypal/Payment.php
+++ Class/Paypal/Payment.php
@@ -2,7 +2,11 @@
 
 namespace Class\Paypal;
 
+use Class\Users\User;
+
 class Payment
 {
-    public function __construct(){}
+    public function __construct(){
+        var_dump(new User());
+    }
 }

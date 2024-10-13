415df55becc2aac0ac9246548f76ac65e15eb831
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-10-13 20:30:00 +0200

    WIP: Donwload the package randomcolor

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 
-// require_once('./Class/Paypal/Payment.php');
-// require_once('./Class/Stripe/Payment.php');
-// require_once('./Class/Users/User.php');
-
 spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    $path = dirname(__DIR__) . '/' . str_replace('\\', '/', $class) . '.php';
     if (file_exists($path)) {
         require_once $path;
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '773074adef726fdf724961859c7538922a084b50',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '773074adef726fdf724961859c7538922a084b50',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mistic100/randomcolor' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '2de67f8e79a8005a3ea95defed1d552bb121de9b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mistic100/randomcolor',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

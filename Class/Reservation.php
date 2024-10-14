548b33905fd2990f3dd9d6f4cf4f877d67d75098
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-14 10:16:11 +0200

    WIP: Delete the classes Cart / Strip / Paypal / Users

---
+++ Class/Reservation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Class;
+
+class Reservation
+{
+    const APPROVAL_PENDING = 'en attente';
+    const APPROVAL_ACCEPTED = 'accepté';
+    const APPROVAL_REJECTED = 'refusé';
+}
--- public/index.php
+++ public/index.php
@@ -1,19 +1,11 @@
 <?php
 
-// spl_autoload_register(function ($class) {
-//     $path = dirname(__DIR__) . '/' . str_replace('\\', '/', $class) . '.php';
-//     if (file_exists($path)) {
-//         require_once $path;
-//     }
-// });
+use Class\Reservation;
 
 require '../vendor/autoload.php';
 
-use Class\Paypal\Payment as PaymentPaypal;
-use Class\Stripe\Payment as PaymentStripe;
-use \Colors\RandomColor;
+$status = 'en attente';
 
-$paymentPaypal = new PaymentPaypal();
-$paymentStripe = new PaymentStripe();
-
-var_dump(RandomColor::one());
+if ($status === Reservation::APPROVAL_PENDING) {
+    echo 'en attente';
+}

7865b0a9f49d7a0b2d5afaf2802ca99215eb648f
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-14 11:48:49 +0200

    WIP: Update index.php to use the enum class OfficeStatus & the class OfficeReservation

--- public/index.php
+++ public/index.php
@@ -5,14 +5,12 @@ use Class\Enums\OfficeStatus;
 
 require '../vendor/autoload.php';
 
-// $status = 'en attente';
 
-// if ($status === Reservation::APPROVAL_PENDING) {
-//     echo 'en attente !';
-// }
+$reservation = new OfficeReservation();
 
-// $reservation = new OfficeReservation();
+if (OfficeStatus::APPROVAL_PENDING === $reservation->status) {
+    echo 'en attente !';
+}
 
-// var_dump($reservation);
 
-var_dump(OfficeStatus::APPROVAL_PENDING);
+var_dump($reservation);

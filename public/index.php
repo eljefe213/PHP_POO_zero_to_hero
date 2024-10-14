2f33165150196402a067e3d32ef3c39ec11d11d8
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-14 16:15:05 +0200

    WIP: Delete the class OfficeReservation and clean up the project

--- public/index.php
+++ public/index.php
@@ -1,10 +1,3 @@
 <?php
 
-use Class\OfficeReservation;
-
 require_once '../vendor/autoload.php';
-
-
-var_dump(OfficeReservation::getInstance());
-var_dump(OfficeReservation::getInstance());
-var_dump(OfficeReservation::getInstance());

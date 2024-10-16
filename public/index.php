485bd43f690a508c461a7c46409430bd775a488a
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-16 16:58:36 +0200

    WIP: Delete the class Vehicule 'Error' from the index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,3 @@
 <?php
 
 require '../vendor/autoload.php';
-
-class Vehicule {
-    public function __construct(protected string $marque) {
-        
-    }
-
-    public function rouler() : string {
-        return 'roule';
-    }
-
-    public function marque() : string {
-        return 'Je roule en' . $this->marque;
-    }
-}

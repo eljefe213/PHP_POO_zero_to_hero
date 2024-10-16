9d4291417248bf046cfa173996571ea671dff69d
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-10-16 22:42:13 +0200

    WIP: Add the new class Renault

--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 
 use Class\Peugeot;
 use Class\Vehicule;
+use Class\Renault;
 
 require '../vendor/autoload.php';
 
 $peugeot = new Peugeot('peugeot');
 var_dump( $peugeot->marque());
 
+$renault = new Renault('renault');
+var_dump( $renault->marque());

b817dd7a4a435213f4fccacc73e47cf486d31ba3
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-17 11:59:49 +0200

    WIP: Clean up the project

--- Class/Peugeot.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Class;
-
-class Peugeot extends Vehicule {
-    public function marque(): string {
-        return 'Je suis un vehicule français et je suis une ' . $this->marque . '.';
-    }
-}
--- Class/Renault.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-namespace Class;
-
-class Renault extends Vehicule {
-    public function marque(): string {
-        return 'Je suis un vehicule moderne et je suis une ' . $this->marque . '.';
-    }
-}

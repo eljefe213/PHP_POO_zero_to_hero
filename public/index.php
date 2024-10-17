692987c89cd743e06d303581035eb48897247b9f
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-17 10:44:15 +0200

    WIP: Delete the use Vehicule on the index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
 
 use Class\Peugeot;
-use Class\Vehicule;
 use Class\Renault;
 
 require '../vendor/autoload.php';
 
 $peugeot = new Peugeot('peugeot');
-var_dump( $peugeot->marque());
+var_dump($peugeot->marque());
 
 $renault = new Renault('renault');
-var_dump( $renault->marque());
+var_dump($renault->marque());

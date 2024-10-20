d54da97d4df55c9dd131898b37cf7dc06c133591
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-10-20 19:43:23 +0200

    WIP: Clear the index file

--- public/index.php
+++ public/index.php
@@ -1,15 +1,6 @@
 <?php
-use AppClass\ParentClass;
-use AppClass\ChildClass;
+
 require '../vendor/autoload.php';
 
 
-$parent = new ParentClass();
-$child = new ChildClass();
-
-// var_dump($parent->getName(), $child->getName());
-
-// var_dump( ParentClass::class, ChildClass::class );
-
-var_dump( $parent->factory(), $child->factory() );
 

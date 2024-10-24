d6d083d4238f587377983fc04257cbd8c7d513f3
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-10-24 21:54:21 +0200

    WIP: Delete the classes

--- public/index.php
+++ public/index.php
@@ -2,14 +2,3 @@
 
 require '../vendor/autoload.php';
 
-
-$espresso = new AppClass\EspressoMachine();
-$irishCoffee = new AppClass\IrishCoffeeMachine();
-$multiCoffee = new AppClass\MultiCoffeeMachine();
-
-var_dump(
-    $espresso->makeEspresso(),
-    $espresso->makeCoffee(),
-    $irishCoffee->makeIrishCoffee(),
-    $irishCoffee->makeCoffee(),
-);

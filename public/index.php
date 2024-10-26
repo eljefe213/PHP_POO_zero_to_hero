0d3cd3fd2d2ee4ec5d808815d899c401d2137b08
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-10-26 19:29:30 +0200

    WIP: Create new instance of login & user

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 
+use AppClass\User;
+use AppClass\Login;
+
 require '../vendor/autoload.php';
 
+$user = new User('admin', 'password');
+$login = new Login($user);
+
+var_dump($login->login());

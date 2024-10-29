0d555046e5b8b5bf5f5e1a8450f6769dfcfc3544
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-29 11:35:25 +0100

    WIP: Add folder views & folder views/home & file index.php inside

---
+++ views/home/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rem consequuntur iure explicabo aliquid quas, aut dicta? Quidem, quisquam dolorum? Accusantium mollitia, explicabo sed nihil fuga itaque voluptatum quos modi perferendis, rerum at reiciendis pariatur atque excepturi et inventore, facilis soluta nam magni. Sequi consequuntur modi natus alias quod illum labore cum ullam deserunt praesentium!</p>
+</body>
+
+</html>
